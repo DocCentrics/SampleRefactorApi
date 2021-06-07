2495117d60cc31d5ebcbb40b6182b54b579616ef
Daniel Chenery <daniel.chenery@doccentrics.com>
2021-06-07 09:57:36 +0100

    Rename

--- SampleRefactorCustomerApi/Program.cs
+++ SampleRefactorCustomerApi/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SampleRefactorCustomerApi
+namespace SampleRefactorApi
 {
     public class Program
     {
